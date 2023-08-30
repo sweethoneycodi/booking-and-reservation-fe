ee24303a5f58db8c716c5f14a2e2521f0a59724a
Aniezue nonso <seamhealthtech02@SeamHealths-MacBook-Pro.local>
2023-08-30 09:44:47 +0100

    login/reg form

--- bookingfe/src/App.jsx
+++ bookingfe/src/App.jsx
@@ -5,12 +5,17 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
 
 function App() {
   return (
 
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+
       </Routes>
 
     
---
+++ bookingfe/src/pages/Registration.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Registration = () => {
+  return (
+    <>
+      <div className="block max-w-md rounded-lg bg-black p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+        <form>
+          <div className="grid grid-cols-2 gap-4">
+            {/*First name input*/}
+            <div className="relative mb-6" data-te-input-wrapper-init="">
+              <input
+                type="text"
+                className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+                id="exampleInput123"
+                aria-describedby="emailHelp123"
+                placeholder="Full name"
+              />
+              <label
+                htmlFor="emailHelp123"
+                className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
+              >
+                Full name
+              </label>
+            </div>
+          </div>
+          {/*Email input*/}
+          <div className="relative mb-6" data-te-input-wrapper-init="">
+            <input
+              type="email"
+              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+              id="exampleInput125"
+              placeholder="Email address"
+            />
+            <label
+              htmlFor="exampleInput125"
+              className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+            >
+              Email address
+            </label>
+          </div>
+          {/*Password input*/}
+          <div className="relative mb-6" data-te-input-wrapper-init="">
+            <input
+              type="password"
+              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+              id="exampleInput126"
+              placeholder="Password"
+            />
+            <label
+              htmlFor="exampleInput126"
+              className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+            >
+              Password
+            </label>
+          </div>
+          {/*Submit button*/}
+          <button
+            type="submit"
+            className="inline-block w-full rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]"
+            data-te-ripple-init=""
+            data-te-ripple-color="light"
+          >
+            Sign up
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Registration;
